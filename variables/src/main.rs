b102809f476a2bbb663a5ca69c96f9dd70f1ece4
jochen <jochen.heckl@gmail.com>
2024-12-27 14:37:44 +0100

    Fix formating

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,20 +1,16 @@
-
-
 fn main() {
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
-    
+
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("3 hours in seconds is {THREE_HOURS_IN_SECONDS}s.");
 
     variable_shadowing();
 }
 
-
-fn variable_shadowing()
-{
+fn variable_shadowing() {
     let x = 5;
 
     let x = x + 1;
