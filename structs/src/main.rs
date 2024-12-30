d90ec939e151a17e7fe281347dc036aacf6809f1
jochen <jochen.heckl@gmail.com>
2024-12-30 08:05:24 +0100

    Add structs crate

---
+++ structs/src/main.rs
@@ -0,0 +1,107 @@
+// fn main() {
+//     let width1 = 30;
+//     let height1 = 50;
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(width1, height1)
+//     );
+// }
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+// fn main() {
+//     let rect = (30, 50);
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(rect)
+//     );
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn has_width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square( size : u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }    
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect)
+    );
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect.area()
+    );
+
+    if rect.has_width() {
+        println!("The rectangle has a nonzero width; it is {}.", rect.width);
+    }
+
+    dbg!(&rect);
+
+    println!("rect is {rect:#?}");
+
+    println!(
+        "The area of a square with side length 5 is {} square pixels.",
+        Rectangle::square(5).area()
+    );
+
+    run_can_hold();
+}
+
+fn run_can_hold() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
