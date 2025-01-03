7266729fcf2932fb28e36abc0222c2b5e0f2b0e0
jochen <jochen.heckl@gmail.com>
2025-01-03 10:35:39 +0100

    Add collections crate

---
+++ collections/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let v = vec![1, 2, 3, 4, 5];
+
+    let _ = v.get(100);
+
+    // let first = &v[0];
+    // v.push(6);
+
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שלום");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+
+}
